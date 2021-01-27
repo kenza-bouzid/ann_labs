18151badc3f05cc6dec0b5b42afcbf57af5f4169
miszkur <miszkurka.aga@gmail.com>
2021-01-27 18:08:25 +0100

    add precision and recall metrics

--- algorithms/metrics.py
+++ algorithms/metrics.py
@@ -1,12 +1,28 @@
 
-import numpy as np 
+import numpy as np
 
-def class_accuracy(y, y_pred, class_label=1):
-  class_samples_num = (y == class_label).sum()
-  correclty_predicted_samples_num = ((y_pred == class_label) & (y_pred == y)).sum()
-  return correclty_predicted_samples_num / class_samples_num
 
-def accuracy(y, y_pred):
-  accA = class_accuracy(y, y_pred, -1)
-  accB = class_accuracy(y, y_pred, 1)
-  print(f'Accuracy for A: {accA}, Accuracy for B: {accB}')
+def class_precision(y, y_pred, class_label=1):
+    correclty_predicted_samples_num = (
+        (y_pred == class_label) & (y_pred == y)).sum()
+    all_predicted_class_num = (y_pred == class_label).sum()
+    return correclty_predicted_samples_num / all_predicted_class_num
+
+
+def precision(y, y_pred):
+    precA = class_precision(y, y_pred, -1)
+    precB = class_precision(y, y_pred, 1)
+    print(f'Precision for A: {precA}, for B: {precB}')
+
+
+def class_recall(y, y_pred, class_label=1):
+    correclty_predicted_samples_num = (
+        (y_pred == class_label) & (y_pred == y)).sum()
+    all_class_samples = (y == class_label).sum()
+    return correclty_predicted_samples_num / all_class_samples
+
+
+def recall(y, y_pred):
+    recA = class_recall(y, y_pred, -1)
+    recB = class_recall(y, y_pred, 1)
+    print(f'Recall for A: {recA}, for B: {recB}')
