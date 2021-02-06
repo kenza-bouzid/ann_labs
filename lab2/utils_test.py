9afe7bb2a578a79c4bb7bc4669b699ab7970d820
Kenzananas <bouzidkenza1@gmail.com>
2021-02-06 15:03:08 +0100

    modular weights

--- lab2/utils_test.py
+++ lab2/utils_test.py
@@ -96,10 +96,10 @@ def plot_estimate(data, type, learning_mode, centers_sampling, n=20, n_iter=3, w
     plt.show()
 
 
-def error_experiment(data, type, learning_mode, centers_sampling):
+def error_experiment(data, type, learning_mode, centers_sampling, weight=1.0, drop=2**9-1, sigma=1.0):
 
     error, n_nodes = experiment_nodes(data, learning_mode=learning_mode,
-                                      centers_sampling=centers_sampling, sigma=1.0)
+                                      centers_sampling=centers_sampling, weight=weight, drop=drop, sigma=sigma)
     plot_error(n_nodes, error, type, learning_mode=learning_mode,
                centers_sampling=centers_sampling)
     print(f"Optimal n_nodes RBF for {type}!")
