e7f6e5b590def429fc197dcb5edce24012292678
Kenzananas <bouzidkenza1@gmail.com>
2021-02-06 12:21:17 +0100

    test refactor

--- lab2/rbf.py
+++ lab2/rbf.py
@@ -5,14 +5,33 @@ from scipy.sparse.construct import rand
 from sklearn.utils import shuffle
 from tqdm import tqdm
 import random
+from enum import Enum
 
+
+class CentersSampling(Enum):
+    LINEAR = 0 
+    WEIGHTED = 1
+    DATA = 2
+
+class LearningMode(Enum):
+    BATCH = 0
+    DELTA = 0
+    HYBRID = 0
 
 class RBF():
-    def __init__(self, n=1, drop=2**9-1, weight=1, sigma=0.5, dim=1):
-        if dim == 1:
-            self.set_centers(n, drop, weight)
-            self.set_sigmas(sigma)
+    def __init__(self, centers_sampling, n_nodes=20, n_inter=1, drop=2**9-1, weight=1.0, x=None, seed=42, sigma=0.5):
+        
+        if centers_sampling == CentersSampling.LINEAR:
+            self.set_linear_centers(n_nodes)
 
+        elif centers_sampling == CentersSampling.WEIGHTED:
+            self.set_centers_weighted(n_inter, drop, weight)
+
+        elif centers_sampling == CentersSampling.DATA:
+            self.set_centers_from_data(x, n_nodes, seed)
+
+        self.sigmas = np.full(self.n_nodes, sigma)
+    
     def compute_phi(self, x):
         n, N = self.centers.shape[0], x.shape[0]
         phi = np.zeros((N, n))
@@ -80,10 +99,10 @@ class RBF():
             X, f, X_test, f_test, lr, max_iters, seed)
         return f_hat, error
 
-    def set_centers(self, n, drop, weight):
-        x_line = np.linspace(0, 2, 9)*np.pi
+    def set_centers_weighted(self, n_inter, drop, weight):
+        x_line = np.linspace(0, 2, 9) * np.pi
 
-        if n == 0:
+        if n_inter == 0:
             self.centers = x_line[1::2]
             self.n_nodes = len(self.centers)
             return
@@ -93,17 +112,19 @@ class RBF():
         for i in range(len(x_line)-1):
             bit = (drop >> (i+1) & 1) != 0
             self.centers = np.append(self.centers, np.linspace(
-                x_line[i], x_line[i+1], num=int(n*bit*weight), endpoint=False))
+                x_line[i], x_line[i+1], num=int(n_inter*bit*weight), endpoint=False))
 
         self.centers = np.sort(self.centers)
         self.n_nodes = len(self.centers)
+    
+    def set_linear_centers(self, n_nodes):
+        self.centers = np.linspace(0, 2*np.pi, n_nodes)
+        self.n_nodes = n_nodes
 
-    def set_sigmas(self, sigma):
-        self.sigmas = np.full(self.n_nodes, sigma)
-
-    def set_centers_from_data(self, x, n_nodes, sigma=1.0, seed=42):
+    def set_centers_from_data(self, x, n_nodes, seed=42):
         random.seed(seed)
         self.n_nodes = n_nodes
         indices = random.sample(range(len(x)), n_nodes)
         self.centers = x[indices]
-        self.set_sigmas(sigma)
+        
+
