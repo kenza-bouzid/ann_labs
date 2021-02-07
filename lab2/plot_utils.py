14db80c9261e94022e6246fbf74b3060e8c692d2
miszkur <miszkurka.aga@gmail.com>
2021-02-07 18:16:08 +0100

    remove commented code from plot utils

--- lab2/plot_utils.py
+++ lab2/plot_utils.py
@@ -5,59 +5,6 @@ from rbf import CentersSampling
 import matplotlib.pyplot as plt
 import numpy as np
 
-# def experiment(data, n_nodes, error, n, weight=1.0):
-#     rbf_net = RBF(n=n, weight=weight)
-#     _, err = rbf_net.batch_learning(
-#         data.x, data.y, data.x_test, data.y_test)
-#     n_nodes.append(rbf_net.n_nodes)
-#     error.append(err)
-
-# def experiment2(data, n_nodes, error, n):
-#     rbf_net = RBF(n=n)
-#     rbf_net.centers = np.linspace(0, 2*np.pi, n)
-#     rbf_net.n_nodes = n
-#     rbf_net.set_sigmas(0.5)
-#     _, err = rbf_net.batch_learning(
-#         data.x, data.y, data.x_test, data.y_test)
-#     n_nodes.append(rbf_net.n_nodes)
-#     error.append(err)
-
-# def experiment_nodes(data):
-#     error = []
-#     n_nodes = []
-#     for i in range(8,40):
-#         # experiment(data, n_nodes, error, n=i, weight=1.0)
-#         # experiment_delta(data, n_nodes, error, n=i, weight=1.0)
-#         # experiment2(data, n_nodes, error, n=i)
-#         experiment2_delta(data, n_nodes, error, n=i)
-
-#     return error, n_nodes
-
-# def experiment_delta(data, n_nodes, error, n, weight=1.0):
-#     rbf_net = RBF(n=n, weight=weight)
-#     _, err = rbf_net.delta_learning(
-#         data.x, data.y, data.x_test, data.y_test)
-#     n_nodes.append(rbf_net.n_nodes)
-#     error.append(err)
-
-# def experiment2_delta(data, n_nodes, error, n):
-#     rbf_net = RBF(n=n)
-#     rbf_net.centers = np.linspace(0, 2*np.pi, n)
-#     rbf_net.n_nodes = n
-#     rbf_net.set_sigmas(0.5)
-#     _, err = rbf_net.delta_learning(
-#         data.x, data.y, data.x_test, data.y_test)
-#     n_nodes.append(rbf_net.n_nodes)
-#     error.append(err)
-
-# def plot_error(n_nodes, error, data):
-#     plt.scatter(n_nodes, error)
-#     plt.xlabel("number of hidden nodes")
-#     plt.ylabel("absolute residual error")
-#     plt.title("Absolute Residual Error vs number of hidden nodes")
-#     plt.legend()
-#     plt.savefig(f"images/3.1/{data}_error2_{len(n_nodes)}.png")
-#     plt.show()
 
 def plot_RBF_grid_search(data):
     n_nodes = [10, 15, 20, 25]
@@ -67,11 +14,13 @@ def plot_RBF_grid_search(data):
     for n in n_nodes:
         for w in weights:
             rbf_net = RBF(CentersSampling.LINEAR, n_nodes=n, sigma=w)
-            y_hat, error = rbf_net.batch_learning(data.x, data.y, data.x_test,data.y_test)
+            y_hat, error = rbf_net.batch_learning(
+                data.x, data.y, data.x_test, data.y_test)
             results[(rbf_net.n_nodes, w)] = error
-    
+
     keys = np.array(list(results.keys()))
-    plt.scatter(keys[:,0], keys[:,1], c=list(results.values()), cmap='tab20b', s=200)
+    plt.scatter(keys[:, 0], keys[:, 1], c=list(
+        results.values()), cmap='tab20b', s=200)
     plt.xlabel('units')
     plt.ylabel('width')
     plt.title('Absolute residual error for different RBF configurations')
@@ -83,14 +32,15 @@ def plot_RBF_grid_search(data):
 def error_estimate_batch(data, n_nodes=20, sigma=0.5):
     rbf_net = RBF(CentersSampling.LINEAR, n_nodes=n_nodes, sigma=0.5)
     y_hat, error = rbf_net.batch_learning(
-            data.x, data.y, data.x_test, data.y_test)
+        data.x, data.y, data.x_test, data.y_test)
     print(f'Error for batch learning: {error}')
-    
+
 
 def error_estimate_delta(data, n_nodes=20, sigma=0.5):
     errors = []
     for seed in range(20):
-        rbf_net = RBF(CentersSampling.LINEAR, seed=seed, n_nodes=n_nodes, sigma=sigma)
+        rbf_net = RBF(CentersSampling.LINEAR, seed=seed,
+                      n_nodes=n_nodes, sigma=sigma)
         y_hat, error = rbf_net.delta_learning(
             data.x, data.y, data.x_test, data.y_test, seed=seed, max_iters=100, lr=0.1)
         errors.append(error)
