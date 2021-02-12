32d9bb6ac4d88ff96ba3f74d9061d9a8c289f001
Kenzananas <bouzidkenza1@gmail.com>
2021-02-12 20:46:42 +0100

    Hopfield network + 3.1

---
+++ lab3/hopfieldNetwork.py
@@ -0,0 +1,75 @@
+import numpy as np 
+from tqdm import tqdm
+
+class HopfieldNetwork():
+    def __init__(self, patterns, seed=42):
+        np.random.seed(seed)
+        self.P, self.N = patterns.shape
+        self.states = patterns
+        self.W = self.states.T @ self.states
+        self.max_iter = int(np.log2(self.N))
+    
+    def check_storage(self):
+        for s in self.states:
+            self.update_rule(s, self.max_iter)
+
+    def is_in_states(self, pattern):
+        for i, s in enumerate(self.states):
+            if np.array_equal(s, pattern):
+                return i, s
+        return None
+
+    def find_attractors(self): 
+        attractors = []
+        for i in range(2**self.N):
+            binary = bin(i)[2:].zfill(self.N)
+            pattern = np.array([int(bit) for bit in binary])
+            pattern[pattern == 0] = -1
+            _, fixed = self.update_rule(pattern, max_iter=self.max_iter, verbose=False)
+            try:
+                ind, state = self.is_in_states(fixed)
+            except:
+                attractors.append(pattern)
+        attractors = np.array(attractors)
+        return attractors
+
+    def print_result(self, iter, pattern):
+        print(f"Fixed Point found after {iter} iterations!")
+        try:
+            ind, state = self.is_in_states(pattern)
+            print(f"Convergence towards stored pattern nb {ind}!")
+            print(state, "\n")
+        except:
+            print(f"This pattern is an attractor!")
+            print(pattern, "\n")
+
+    def update_rule(self, pattern, max_iter, sync=True, verbose=True):
+        inter_patterns = []
+        for i in range(max_iter):
+            inter_patterns.append(pattern)
+            if sync:
+                pattern = np.sign(self.W @ pattern)
+            else: 
+                ind = np.random.randint(0, self.N, 1)
+                pattern[ind] = np.sign(self.W[ind,:] @ pattern)
+        if verbose:
+            self.print_result(i, pattern)
+        return inter_patterns, pattern
+
+    def update_rule_with_energy(self, pattern, max_iters, sync=True):
+        old_pattern = pattern
+        for i in range(max_iters):
+            if sync:
+                pattern = np.sign(self.W @ pattern)
+            else:
+                ind = np.random.randint(0, self.N, 1)
+                pattern[ind] = np.sign(self.W[ind, :] @ pattern)
+            if pattern == old_pattern:
+                break
+        self.print_result(i, pattern)
+        pass
+
+    def energy(self, state_ind):
+        return -0.5 * self.states[state_ind] @ self.W @ self.states[state_ind]
+
+    
