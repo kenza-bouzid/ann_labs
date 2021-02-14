7122bff0e50861ffb1bd3774eecab960fb9f092e
Tobi-r9 <tobihoppe@gmail.com>
2021-02-14 10:13:31 +0100

    add 3_5 and 3_6 as well as some utility functions

--- lab3/hopfieldNetwork.py
+++ lab3/hopfieldNetwork.py
@@ -2,20 +2,39 @@ import numpy as np
 from tqdm import tqdm
 
 class HopfieldNetwork():
-    def __init__(self, patterns, seed=42):
+    def __init__(self, patterns, seed=42, sparse=False, bias=0):
         np.random.seed(seed)
+        self.sparse = sparse
         self.P, self.N = patterns.shape
         self.states = patterns
-        self.W = self.states.T @ self.states
+        self.W = self.compute_wheight_matrix()
         self.max_iter = int(np.log2(self.N))
+        self.bias = bias
     
+    def zero_self_connection(self):
+        for i in range(self.W.shape[0]):
+            self.W[i,i] = 0
+
+
+    def compute_wheight_matrix(self):
+        if self.sparse:
+            p = 1/(self.P* self.N) * np.sum(self.states)
+            return (self.states.T-p) @ (self.states-p)
+        else:
+            return self.states.T @ self.states
+
+
     def check_storage(self):
         for s in self.states:
             self.update_rule(s, self.max_iter)
     
+
     def check_capacity(self):
+        count = 0
         for s in self.states:
-            self.update_rule(s, self.max_iter)
+            _, new_pattern, _ = self.update_rule(s, 1, verbose=False)
+            count = count + np.array_equal(new_pattern, s)
+        return count   
 
     def is_in_states(self, pattern):
         for i, s in enumerate(self.states):
@@ -55,10 +74,14 @@ class HopfieldNetwork():
         for i in range(max_iter):
             inter_patterns.append(old_pattern)
             if sync:
-                new_pattern = np.sign(self.W @ old_pattern)
+                new_pattern = np.sign(self.W @ old_pattern-self.bias)
+                if self.sparse:
+                    new_pattern = 0.5 + 0.5 * new_pattern
             else: 
                 ind = np.random.randint(0, self.N, 1)
-                new_pattern[ind] = np.sign(self.W[ind,:] @ old_pattern)
+                new_pattern[ind] = np.sign(self.W[ind,:] @ old_pattern-self.bias)
+                if self.sparse:
+                    new_pattern[ind] = 0.5 + 0.5 * new_pattern[ind]
                 
             new_energy = self.energy(old_pattern)
             # if i > 1 and new_energy == energy[-1]:
