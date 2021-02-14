102e256d53849b0baf0ae08cccbbed30e5082f78
miszkur <miszkurka.aga@gmail.com>
2021-02-14 17:15:35 +0100

    Check different seeds for adding noise

--- lab3/utils.py
+++ lab3/utils.py
@@ -3,10 +3,9 @@ import matplotlib.pyplot as plt
 
 
 def add_noise(pattern, noise_frac=0.0, seed=42):
-    np.random.seed(seed)
     flips_num = int(pattern.shape[0] * noise_frac)
-    to_flip = np.random.randint(
-        low=0, high=pattern.shape[0], size=(flips_num))
+    to_flip = np.random.default_rng(seed).choice(
+        pattern.shape[0], size=(flips_num), replace=False)
     noisy_pattern = pattern.copy()
     noisy_pattern[to_flip] = noisy_pattern[to_flip]*-1
     return noisy_pattern
@@ -17,16 +16,21 @@ def plot_distortion_resistance(hop_net, patterns, pattern_num, steps=1):
     is_noise_removed = []
     noise_levels = np.linspace(0, 1, 101)
     for noise in noise_levels:
-        noisy_pattern = add_noise(pattern, noise)
-        inter, patt, energy = hop_net.update_rule(
-            noisy_pattern, steps, verbose=False)
-        is_noise_removed.append(np.array_equal(pattern, patt))
+        is_removed_seeds = []
+        for seed in range(50):
+            noisy_pattern = add_noise(pattern, noise, seed=seed)
+            inter, patt, energy = hop_net.update_rule(
+                noisy_pattern, steps, verbose=False)
+            is_removed_seeds.append(np.array_equal(pattern, patt))
+                
+        is_noise_removed.append(np.mean(is_removed_seeds))
 
     plt.plot(noise_levels, is_noise_removed)
     plt.xlabel('Noise level')
     plt.ylabel('Converged 0/1')
     plt.title(
-        f'Disotrtion resistance for pattern {pattern_num}, until {is_noise_removed.index(False)-1}% noise')
+        f'Distortion resistance for pattern {pattern_num}, mean: {round(np.mean(is_noise_removed), 4)}')
+        # f'Distortion resistance for pattern {pattern_num}, until {np.where(np.array(is_noise_removed) < 1)[0][0]}% noise')
 
 
 def plot_attractor_for_pattern(hop_net, patterns, pattern_num, noise, steps=1):
@@ -35,3 +39,4 @@ def plot_attractor_for_pattern(hop_net, patterns, pattern_num, noise, steps=1):
     inter, patt, energy = hop_net.update_rule(
         noisy_pattern, steps, verbose=False)
     plt.imshow(patt.reshape((32, 32)), cmap="binary")
+    return patt
