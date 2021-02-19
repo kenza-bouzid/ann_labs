b0732b548102e3cdac9e19df98d80435f4761ff5
miszkur <miszkurka.aga@gmail.com>
2021-02-19 19:41:19 +0100

    4.1 started

--- lab4/dbn.py
+++ lab4/dbn.py
@@ -108,7 +108,7 @@ class DeepBeliefNet():
             
             records.append( [ ax.imshow(vis.reshape(self.image_size), cmap="bwr", vmin=0, vmax=1, animated=True, interpolation=None) ] )
             
-        anim = stitch_video(fig,records).save("hits/%s.generate%d.mp4"%(name,np.argmax(true_lbl)))            
+        anim = stitch_video(fig,records).save("hist/%s.generate%d.mp4"%(name,np.argmax(true_lbl)))            
             
         return
 
--- lab4/run.py
+++ lab4/run.py
@@ -1,15 +1,16 @@
 from util import *
-from rbm import RestrictedBoltzmannMachine 
+from rbm import RestrictedBoltzmannMachine
 from dbn import DeepBeliefNet
 
 if __name__ == "__main__":
 
-    image_size = [28,28]
-    train_imgs,train_lbls,test_imgs,test_lbls = read_mnist(dim=image_size, n_train=60000, n_test=10000)
+    image_size = [28, 28]
+    train_imgs, train_lbls, test_imgs, test_lbls = read_mnist(
+        dim=image_size, n_train=60000, n_test=10000)
 
     ''' restricted boltzmann machine '''
-    
-    print ("\nStarting a Restricted Boltzmann Machine..")
+
+    print("\nStarting a Restricted Boltzmann Machine..")
 
     rbm = RestrictedBoltzmannMachine(ndim_visible=image_size[0]*image_size[1],
                                      ndim_hidden=200,
@@ -18,42 +19,44 @@ if __name__ == "__main__":
                                      is_top=False,
                                      n_labels=10,
                                      batch_size=10
-    )
-    
+                                     )
+
     rbm.cd1(visible_trainset=train_imgs, n_iterations=10000)
-    
-    ''' deep- belief net '''
 
-    print ("\nStarting a Deep Belief Net..")
-    
-    dbn = DeepBeliefNet(sizes={"vis":image_size[0]*image_size[1], "hid":500, "pen":500, "top":2000, "lbl":10},
-                        image_size=image_size,
-                        n_labels=10,
-                        batch_size=10
-    )
-    
-    ''' greedy layer-wise training '''
+    # ''' deep- belief net '''
 
-    dbn.train_greedylayerwise(vis_trainset=train_imgs, lbl_trainset=train_lbls, n_iterations=10000)
+    # print("\nStarting a Deep Belief Net..")
 
-    dbn.recognize(train_imgs, train_lbls)
-    
-    dbn.recognize(test_imgs, test_lbls)
+    # dbn = DeepBeliefNet(sizes={"vis": image_size[0]*image_size[1], "hid": 500, "pen": 500, "top": 2000, "lbl": 10},
+    #                     image_size=image_size,
+    #                     n_labels=10,
+    #                     batch_size=10
+    #                     )
 
-    for digit in range(10):
-        digit_1hot = np.zeros(shape=(1,10))
-        digit_1hot[0,digit] = 1
-        dbn.generate(digit_1hot, name="rbms")
+    # ''' greedy layer-wise training '''
 
-    ''' fine-tune wake-sleep training '''
+    # dbn.train_greedylayerwise(vis_trainset=train_imgs,
+    #                           lbl_trainset=train_lbls, n_iterations=10000)
 
-    dbn.train_wakesleep_finetune(vis_trainset=train_imgs, lbl_trainset=train_lbls, n_iterations=10000)
+    # dbn.recognize(train_imgs, train_lbls)
 
-    dbn.recognize(train_imgs, train_lbls)
-    
-    dbn.recognize(test_imgs, test_lbls)
-    
-    for digit in range(10):
-        digit_1hot = np.zeros(shape=(1,10))
-        digit_1hot[0,digit] = 1
-        dbn.generate(digit_1hot, name="dbn")
+    # dbn.recognize(test_imgs, test_lbls)
+
+    # for digit in range(10):
+    #     digit_1hot = np.zeros(shape=(1, 10))
+    #     digit_1hot[0, digit] = 1
+    #     dbn.generate(digit_1hot, name="rbms")
+
+    # ''' fine-tune wake-sleep training '''
+
+    # dbn.train_wakesleep_finetune(
+    #     vis_trainset=train_imgs, lbl_trainset=train_lbls, n_iterations=10000)
+
+    # dbn.recognize(train_imgs, train_lbls)
+
+    # dbn.recognize(test_imgs, test_lbls)
+
+    # for digit in range(10):
+    #     digit_1hot = np.zeros(shape=(1, 10))
+    #     digit_1hot[0, digit] = 1
+    #     dbn.generate(digit_1hot, name="dbn")
